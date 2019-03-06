70adadbf10879ab8359ad810e3a4f4648a5a99f6
root <root@profeN2I.informatica.escoladeltreball.org>
2019-03-06 13:25:35 +0100

    net host

---
+++ README.md
@@ -0,0 +1,7 @@
+@ edt ASIX M11-SAD
+## Escola del treball Curs 2018-2019
+
+### net18
+
+repositori per a serveis de xarxa.
+
---
+++ net18:nethost/startup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+/opt/docker/install.sh && echo "Ok install"
+/usr/sbin/httpd
+/usr/sbin/xinetd -dontfork && echo "xinetd OK"
+
