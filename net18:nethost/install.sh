4de9c8e8e97e365ede6f6f2ef68a0c5f8cd88d57
Eduard Canet <ecanet@profeN2I.informatica.escoladeltreball.org>
2019-03-07 09:38:42 +0100

    kerberos

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 echo "@edt ASIX M11-SAD" > /var/www/index.html
+echo "Benvinguts al vsftpd" > /var/ftp/hola.pdf
+echo "directori public" > /var/ftp/pub/info.txt
 cp /opt/docker/* /etc/xinetd.d/
--- net18:nethost/startup.sh
+++ net18:nethost/startup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 /opt/docker/install.sh && echo "Ok install"
 /usr/sbin/httpd
+/usr/sbin/vsftpd
 /usr/sbin/xinetd -dontfork && echo "xinetd OK"
 
