ebbd340c056118f31118b30cbf04ed6709e7fd1c
Fedora <fedora@ip-172-31-20-75.eu-west-2.compute.internal>
2019-03-07 17:51:14 +0000

    net host

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -2,4 +2,5 @@
 echo "@edt ASIX M11-SAD" > /var/www/index.html
 echo "Benvinguts al vsftpd" > /var/ftp/hola.pdf
 echo "directori public" > /var/ftp/pub/info.txt
+/usr/bin/ssh-keygen -A
 cp /opt/docker/* /etc/xinetd.d/
--- net18:nethost/startup.sh
+++ net18:nethost/startup.sh
@@ -2,5 +2,6 @@
 /opt/docker/install.sh && echo "Ok install"
 /usr/sbin/httpd
 /usr/sbin/vsftpd
+/usr/sbin/sshd
 /usr/sbin/xinetd -dontfork && echo "xinetd OK"
 
