536ef5d1db348aa8c1837863a1549154c531c97f
edtasixm06 <asixm06@edt.org>
2019-03-08 17:32:56 +0100

    net host

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -3,4 +3,4 @@ echo "@edt ASIX M11-SAD" > /var/www/index.html
 echo "Benvinguts al vsftpd" > /var/ftp/hola.pdf
 echo "directori public" > /var/ftp/pub/info.txt
 /usr/bin/ssh-keygen -A
-cp /opt/docker/* /etc/xinetd.d/
+cp /opt/docker/xinetd.d* /etc/xinetd.d/
